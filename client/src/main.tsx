7295e146dc0d83ab4e96d9d1cfda9548b71f083d
Shane Nelson <shane.nelson@leg.bc.ca>
2026-03-22 14:08:22 -0700

    Wrap app in apollo provider so we can use apollo queries to get data from the server

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import '@mantine/core/styles.css'
 import '@mantine/charts/styles.css'
 import '@mantine/notifications/styles.css'
+import { ApolloProvider } from '@apollo/client/react'
 import { ThemeProfileProvider } from './components/ThemeProfileContext.tsx'
 import { ThemedApp } from './ThemedApp.tsx'
+import { apolloClient } from './apollo/client.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProfileProvider>
-      <ThemedApp />
-    </ThemeProfileProvider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeProfileProvider>
+        <ThemedApp />
+      </ThemeProfileProvider>
+    </ApolloProvider>
   </StrictMode>,
 )
